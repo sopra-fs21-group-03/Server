c7e288bdff80cfbe606c25bbbaafefb67fb1d16b
mr-carlitos <carlos.kirchdorfer@uzh.ch>
2021-04-01 10:08:38 +0200

    Addes UserPutDTO, started with the Fold functionality
    This took me about 20min

--- src/main/java/ch/uzh/ifi/hase/soprafs21/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/controller/GameController.java
@@ -1,4 +1,25 @@
 package ch.uzh.ifi.hase.soprafs21.controller;
+import ch.uzh.ifi.hase.soprafs21.rest.dto.UserPutDTO;
+import ch.uzh.ifi.hase.soprafs21.service.GameService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class GameController {
+    private final GameService gameService;
+
+    GameController(GameService gameService){this.gameService = gameService;}
+
+    @PutMapping("/games/{UserID}/fold")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    public void userfolds(@PathVariable("UserID") Long id, @RequestBody UserPutDTO userPutDTO){
+
+
+    }
+
+
+
+
+
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs21/service/GameService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/service/GameService.java
@@ -1,4 +1,10 @@
 package ch.uzh.ifi.hase.soprafs21.service;
 
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class GameService {
 }
