dc6668a3d7a5b871317c9caa605bee6bc8b167c6
Luca Huber <luca.huber2@uzh.ch>
2021-04-20 18:08:05 +0200

    make test work again

--- src/test/java/ch/uzh/ifi/hase/soprafs21/entity/GameEntityTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/entity/GameEntityTest.java
@@ -164,8 +164,7 @@ class GameEntityTest {
         }
 
     }
-
-/*
+    
     @Test
     void blindRoleWillBeReassigned_success() {
         //After a gameround, each Blind role will be reassigned to the left player of the last Blind
@@ -180,7 +179,6 @@ class GameEntityTest {
         assertEquals(Blind.BIG, getUser_providedUsername_inallUsers(userOnTurn).getBlind());
 
     }
-*/
 
     @Test
     void getUsernameOfPotentialNextUserInTurn() {
