310bbf1094bda45b6fc5b552cdf5513bd45cbf28
mr-carlitos <carlos.kirchdorfer@uzh.ch>
2021-04-22 16:32:57 +0200

    New Elements

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/game/protocol/LogElement.java
@@ -0,0 +1,8 @@
+package ch.uzh.ifi.hase.soprafs21.game.protocol;
+
+public class LogElement implements ProtocolElement{
+    @Override
+    public String getAsString() {
+        return null;
+    }
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/game/protocol/ProtocolElement.java
@@ -0,0 +1,5 @@
+package ch.uzh.ifi.hase.soprafs21.game.protocol;
+
+public interface ProtocolElement {
+    String getAsString();
+}
